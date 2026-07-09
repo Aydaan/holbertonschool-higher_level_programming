2bb537d82e9a4809b31bd69c50c1ac911df0832f
Aydaan <13565@holbertonstudents.com>
2026-07-09 17:10:45 +0400

    fixed

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -7,17 +7,18 @@ Module that contains the matrix_divided function.
 def matrix_divided(matrix, div):
     """
     Divide all elements of a matrix by div.
+
     Returns a new matrix with elements rounded to 2 decimal places.
     """
+
     if (not isinstance(matrix, list) or
-        not all(isinstance(row, list) for row in matrix) or
-        not all(all(isinstance(x, (int, float)) for x in row)
-                for row in matrix)):
-    raise TypeError(
-        "matrix must be a matrix (list of lists) of integers/floats"
-    )
-if matrix == [] or matrix == [[]]:
-    return []
+            matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all(all(isinstance(x, (int, float)) for x in row)
+                    for row in matrix)):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
